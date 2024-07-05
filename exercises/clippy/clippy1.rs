b59e6573453c15644295f0b1df58baf6a29a343b
fgh1999 <fgh@stu.pku.edu.cn>
2024-07-06 01:24:26 +0800

    wired

--- exercises/clippy/clippy1.rs
+++ exercises/clippy/clippy1.rs
@@ -10,10 +10,8 @@
 // hint.
 
 
-use std::f32;
-
 fn main() {
-    let pi = f32::consts::PI;
+    let pi = std::f32::consts::PI;
     let radius = 5.00f32;
 
     let area = pi * f32::powi(radius, 2);
